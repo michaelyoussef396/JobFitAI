40daee4a9ab821b9da28965f9dd8a23687726843
michael youssef <michaelyoussef396@gmail.com>
2024-10-09 23:15:59 +1100

    created navbar structure

--- Client/src/sections/Header.jsx
+++ Client/src/sections/Header.jsx
@@ -3,10 +3,10 @@ import { images } from '../Constants'
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <a href="">
-            <img src={images.logo} alt="" />
+    <header className='fixed top-0 left-0 z-50 w-full py-10'>
+      <div className='flex container h-14 items-center max-lg:px-5'>
+        <a href="/" className='lg:hidden flex-1 cursor-pointer z-2'>
+            <img src={images.logo} alt="logo" width={115} height={115}/>
         </a>
     </div>
     </header>
