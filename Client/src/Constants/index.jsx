6b7057fe81a07507447e7c2932dd28afe49e4d71
michael youssef <michaelyoussef396@gmail.com>
2024-10-10 15:58:10 +1100

    created the outline for the hero page

--- Client/src/Constants/index.jsx
+++ Client/src/Constants/index.jsx
@@ -1,6 +1,14 @@
 import logo from "../images/JobFitAi.png";
 import name from "../images/name.png";
+import bgOutline from "../images/bg-outlines.svg"
+import bgOutlineFill from "../images/bg-outlines-fill.png";
+import close from "../images/close.svg";
+import magic from "../images/magic.svg";
 export const images = {
     logo,
-    name
+    name,
+    bgOutline,
+    bgOutlineFill,
+    close,
+    magic,
 }
