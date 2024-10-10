e72cded1ed4299b345a4ffb28bf1e7fb13e8dc2f
michael youssef <michaelyoussef396@gmail.com>
2024-10-10 16:03:38 +1100

    created resuable button component

--- Client/src/sections/Hero.jsx
+++ Client/src/sections/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Element, Link as LinkScroll } from 'react-scroll'
+import Button from '../components/Button'
+import { images } from '../Constants'
 const Hero = () => {
   return (
     <section className='relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 border-amber-400'>
@@ -10,7 +12,7 @@ const Hero = () => {
                 <h1 className='mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12'>Amazingly Simple</h1>
                 <p className='max-w-440 mb-14 body-1 max-md:mb-10'>We designed JobFitAi to be an easy to use, quick to learn and surprisingly powerful</p>
                 <LinkScroll to="features" offset={-100} spy smooth>
-                    Try it now
+                    <Button icon={images.Zap}>Try it now</Button>
                 </LinkScroll>
             </div>
         </div>
