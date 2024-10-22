20840643d83c5356c99faacee0c5a996cf676dce
michael youssef <michaelyoussef396@gmail.com>
2024-10-22 15:51:23 +1100

    created user table

---
+++ server/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from config import db
+
+# Models go here!
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # Method to set the password
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # Method to check the password
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.name}>'
